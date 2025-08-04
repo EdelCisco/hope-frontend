fefc363c9ea855bf152dc11e729149d999ea9c69
EdelCisco <edelalmeida84gmail.com>
2025-08-04 18:57:21 +0200

    test

--- src/code.tsx
+++ src/code.tsx
@@ -71,6 +71,7 @@ function Code() {
     setMessage('');
     try {
       setBloque(true);
+      alert("ok")
       const response = await api.post("/Code", currentData);
       alert(response.data.route)
       if (response.data.errors == null) {
