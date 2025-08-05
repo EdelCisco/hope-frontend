7abe37ec7cb231a618a02c3a89b066c94ed10bfe
EdelCisco <edelalmeida84gmail.com>
2025-08-05 15:04:33 +0200

    test7

--- src/Profil.tsx
+++ src/Profil.tsx
@@ -103,14 +103,15 @@ if (!user) return <Navigate to="/Connexion" replace />;
         <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-0'>
         
         <div className=' bg-cover bg-center flex flex-col justify-center items-center text-white rounded-md ' style={{backgroundImage: "url('/r.jpeg')"}}>
-          <div className='backdrop-brightness-50 w-full h-full flex flex-col justify-center items-center py-8 md:py-10 lg:py-14 rounded-md'>
+          <div className='backdrop-brightness-50 w-full h-full flex flex-col justify-center items-center py-8  rounded-md'>
             <img src="/buser.png" alt="profil" className='w-24 h-24 rounded-full border-1 border-[#088cb4] my-4 ' />
             <h1 className='font-bold text-2xl xl:text-3xl'> {user.Nom}</h1>
             <p className='py-2 mx-10 text-center lg:text-lg xl:text-xl '>Votre espace profil, où vous pouvez comsulter et modifier vos Informations selon votre convenance</p>
-          </div>
-           <div className='flex items-center justify-end'>
+          <div className='flex items-end justify-end pt-4'>
           <button onClick={supprimer} className='mx-10 rounded-sm text-white font-medium text-center  bg-red-600 shadow-lg py-2 px-4 my-4'> Supprimer le compte</button>
         </div>
+          </div>
+      
         </div>
        
         
