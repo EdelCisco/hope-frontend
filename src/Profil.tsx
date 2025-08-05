844a19337ecb8c65bcacd9238b2d53f8a59d4144
EdelCisco <edelalmeida84gmail.com>
2025-08-05 14:21:23 +0200

    test7

--- src/Profil.tsx
+++ src/Profil.tsx
@@ -103,7 +103,7 @@ if (!user) return <Navigate to="/Connexion" replace />;
         <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-0'>
         <div>
         <div className=' bg-cover bg-center flex flex-col justify-center items-center text-white rounded-md py-4' style={{backgroundImage: "url('/r.jpeg')"}}>
-          <div className='backdrop-brightness-50 w-full h-full flex flex-col justify-center items-center py-8 rounded-md'>
+          <div className='backdrop-brightness-50 w-full h-full flex flex-col justify-center items-center py-4 rounded-md'>
             <img src="/buser.png" alt="profil" className='w-24 h-24 rounded-full border-1 border-[#088cb4] my-4' />
             <h1 className='font-bold text-2xl xl:text-3xl'> {user.Nom}</h1>
             <p className='py-2 mx-10 text-center lg:text-lg xl:text-xl '>Votre espace profil, où vous pouvez comsulter et modifier vos Informations selon votre convenance</p>
@@ -229,7 +229,7 @@ if (!user) return <Navigate to="/Connexion" replace />;
                
               </div>
              </div>}
-            <p onClick={()=> setBloque(!bloque)} className='mx-10 rounded-sm text-white font-medium flex justify-center items-center bg-gradient-to-r from-[#088cb4] to-[#f7941d]  py-2 sm:py-0 shadow-md shadow-gray-300 hover:scale-105 hover:from-[#f7941d] hover:to-[#088cb4] cursor-pointer'> {bloque ? 'Retour' : 'Compléter ou modifier le profil'}</p>
+            <p onClick={()=> setBloque(!bloque)} className='mx-10 rounded-sm text-white font-medium flex justify-center items-center bg-gradient-to-r from-[#088cb4] to-[#f7941d]  py-2  shadow-md shadow-gray-300 hover:scale-105 hover:from-[#f7941d] hover:to-[#088cb4] cursor-pointer'> {bloque ? 'Retour' : 'Compléter ou modifier le profil'}</p>
             {bloque && (
               <button onClick={submit}  className="mx-10 rounded-sm text-white font-medium flex justify-center-safe items-center r bg-gradient-to-r from-[#088cb4] to-[#f7941d] py-2 shadow-md shadow-gray-300 hover:scale-105 hover:from-[#f7941d] hover:to-[#088cb4] cursor-pointer">
                 Confirmer
